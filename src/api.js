f713c54774b359732f8623abc21c23b3e181cbf4
Trinity <amoahtnt@gmail.com>
2022-09-18 13:33:35 +0000

    check

--- src/api.js
+++ src/api.js
@@ -4,19 +4,37 @@ const serverless = require("serverless-http");
 const todoRouter = require("../routes/todo_route");
 const app = express();
 const port = 5000;
+const router = express.Router()
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-app.use("/.netlify/functions/api", todoRouter);
+router.get('/',(req,res)=>{
+  res.json([
+    {
+      name:"alex",
+      age:40
+    }
+  ])
+})
+router.post('/',(req,res)=>{
+  res.json([
+    {
+      name:"alex",
+      age:40
+    }
+  ])
+})
+
 try {
   db().then((e) => {
     app.listen(port, () =>
       console.log("> Server is up and running on port : " + port)
-    );
-  });
+      );
+    });
 } catch (error) {
     throw error.message;
 }
+app.use("/.netlify/functions/api", router);
 
 module.exports.handler = serverless(app);
