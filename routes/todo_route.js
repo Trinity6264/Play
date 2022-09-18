bf0aa14e1701dcce7a5a48af01dac0c919bdf75d
Trinity <amoahtnt@gmail.com>
2022-09-18 13:42:13 +0000

    check

--- routes/todo_route.js
+++ routes/todo_route.js
@@ -1,6 +1,6 @@
-const todoRouter = require("express").Router();
+const express = require("express");
 const { addTodo, getTodo,play } = require("../controller/todo_controller");
-
+const todoRouter = express.Router();
 todoRouter.get("/", getTodo);
 
 todoRouter.post("/", addTodo);
