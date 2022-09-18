1aff1836a13d71d64bb3058fc79ef11113b0c9d2
Trinity <amoahtnt@gmail.com>
2022-09-18 14:01:24 +0000

    change mongo timeout

--- config/db.js
+++ config/db.js
@@ -3,21 +3,20 @@ const mongoose = require("mongoose");
 // awGcFY7lmA0RPCaY
 // autumshipmentalics
 const db = async () => {
-  await mongoose.connect(
-    "mongodb+srv://autumshipmentalics:awGcFY7lmA0RPCaY@cluster0.260w81r.mongodb.net/",
-    {
-      useNewUrlParser: true,
-      serverSelectionTimeoutMS: 9000
-    },
-    (err) => {
-      if (!err) {
-        console.log("MongoDB Connection Succeeded.");
-      } else {
-        console.log("Error in DB connection: " + err);
-      }
-    }
-  );
-  mongoose.disconnect();
-};
+  try {
+    const resp = await mongoose.connect(
+      "mongodb+srv://autumshipmentalics:awGcFY7lmA0RPCaY@cluster0.260w81r.mongodb.net/"
+    );
+    if(mongoose.ConnectionStates.connected){
+      console.log("Database opend");
+      
+    }else{
+      console.log(mongoose.ConnectionStates);
 
-module.exports = { db };
+    }
+  } catch (error) {
+    console.log(error);
+  }
+};
+db();
+
--- src/api.js
+++ src/api.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const { db } = require("../config/db");
+require("../config/db");
 const serverless = require("serverless-http");
 const todoRouter = require("../routes/todo_route");
 const app = express();
@@ -26,15 +26,7 @@ router.post('/',(req,res)=>{
   ])
 })
 
-try {
-  db().then((e) => {
-    app.listen(port, () =>
-      console.log("> Server is up and running on port : " + port)
-    );
-  });
-} catch (error) {
-    throw error.message;
-}
+
 
 app.use("/.netlify/functions/api", todoRouter);
 module.exports.handler = serverless(app);
